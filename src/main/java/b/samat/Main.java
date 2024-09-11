554ee34494453e94c51d1700a353d529e83e8fb1
Samat <kosbarmak3@gmail.com>
2024-09-11 21:10:21 +0500

    conflict in Main.java

--- src/main/java/b/samat/Main.java
+++ src/main/java/b/samat/Main.java
@@ -2,6 +2,6 @@ package b.samat;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello KOSTANAY!!");
+        System.out.println("Hello from new-branch");
     }
 }

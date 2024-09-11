69f3c4aad3f272c416aa8497b5ffa685234a29b4
Samat <kosbarmak3@gmail.com>
2024-09-11 14:15:34 +0500

    Изменения в Main.java перед переключением на master

--- src/main/java/b/samat/Main.java
+++ src/main/java/b/samat/Main.java
@@ -2,6 +2,6 @@ package b.samat;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Almaty!");
+        System.out.println("Hello Almaty!!!");
     }
 }
